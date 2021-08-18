5e6c55b3522d213ec07e95e7fe00b1b23d1c7c90
Sayeddilawar <sayeddilawar631@gmail.com>
2021-08-18 21:50:34 +0530

    Requirement 2 : Area and Perimeter of Rectangle.

--- src/main/java/OperationsOfSquareAndRectangleMain.java
+++ src/main/java/OperationsOfSquareAndRectangleMain.java
@@ -1,15 +1,30 @@
 public class OperationsOfSquareAndRectangleMain
 {
     double side;
+    int length;
+    int width;
+
     public OperationsOfSquareAndRectangleMain(double side)
     {
         this.side = side;
     }
     public double areaOfSquare() {
         return side*side;
-
     }
     public double perimeterOfSquare() {
         return 4*side;
+    }
+
+    public OperationsOfSquareAndRectangleMain(int length,int width)
+    {
+        this.length=length;
+        this.width = width;
+    }
+    public int areaOfRectangle() {
+        return length*width;
+    }
+
+    public int perimeterOfRectangle() {
+        return 2*(length+width);
     }
 }
--- src/test/java/OperationsOnSquareAndRectangle.java
+++ src/test/java/OperationsOnSquareAndRectangle.java
@@ -1,75 +1,112 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OperationsOnSquareAndRectangle
 {
-    @DisplayName("Area of Square with Positive Sides")
-    @Test
-    void toImplementAreaOfSquareWithPositiveSides()
-    {
-        OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(5);
-        assertEquals(25,object.areaOfSquare());
+    @Nested
+    public class squareOperations {
+        @DisplayName("Area of Square with Positive Sides")
+        @Test
+        void toImplementAreaOfSquareWithPositiveSides()
+        {
+            OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(5);
+            assertEquals(25, object.areaOfSquare());
+        }
+
+        @DisplayName("Area of Square with Negative Sides")
+        @Test
+        void toImplementAreaOfSquareWithNegativeSides()
+        {
+            OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(-5);
+            assertEquals(25, object.areaOfSquare());
+        }
+
+        @DisplayName("Area of Square with Positive Decimal side")
+        @Test
+        void toImplementAreaOfSquareWithPositiveDecimalSides()
+        {
+            OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(5.5);
+            assertEquals(30.25, object.areaOfSquare());
+        }
+
+        @DisplayName("Area of Square with Positive Decimal side")
+        @Test
+        void toImplementAreaOfSquareWithNegativeDecimalSides()
+        {
+            OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(-5.5);
+            assertEquals(30.25, object.areaOfSquare());
+        }
+
+        //Perimeter of Square
+        @DisplayName("Perimeter of Square with Positive side")
+        @Test
+        void toImplementPerimeterOfSquareWithPositiveSides()
+        {
+            OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(10);
+            assertEquals(40, object.perimeterOfSquare());
+        }
+
+        @DisplayName("Perimeter of Square with Negative side")
+        @Test
+        void toImplementPerimeterOfSquareWithNegativeSides() {
+            OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(-10);
+            assertEquals(-40, object.perimeterOfSquare());
+        }
+
+        @DisplayName("Perimeter of Square with Positive Decimal side")
+        @Test
+        void toImplementPerimeterOfSquareWithPositiveDecimalSides() {
+            OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(2.5);
+            assertEquals(10, object.perimeterOfSquare());
+        }
+
+        @DisplayName("Perimeter of Square with Negative Decimal side")
+        @Test
+        void toImplementPerimeterOfSquareWithNegativeDecimalSides() {
+            OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(-2.5);
+            assertEquals(-10, object.perimeterOfSquare());
+        }
+        //End Perimeter Of Square
+
     }
-
-    @DisplayName("Area of Square with Negative Sides")
-    @Test
-    void toImplementAreaOfSquareWithNegativeSides()
+    @Nested
+    public class areaAndPerimeterOfRectangle
     {
-        OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(-5);
-        assertEquals(-25,object.areaOfSquare());
+        @DisplayName("Area of Rectangle with Positive Values")
+        @Test
+        void toImplementAreaOfRectangleWithPositiveDimensions()
+        {
+            OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(4,6);
+            assertEquals(24, object.areaOfRectangle());
+        }
+
+        @DisplayName("Area of Rectangle with Negative Values")
+        @Test
+        void toImplementAreaOfRectangleWithNegativeDimensions()
+        {
+            OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(-4,-6);
+            assertEquals(24, object.areaOfRectangle());
+        }
+
+        @DisplayName("Perimeter of Rectangle with Positive Values")
+        @Test
+        void toImplementPerimeterOfRectangleWithPositiveDimensions()
+        {
+            OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(4,6);
+            assertEquals(20, object.perimeterOfRectangle());
+        }
+
+        @DisplayName("Perimeter of Rectangle with Negative Values")
+        @Test
+        void toImplementPerimeterOfRectangleWithNegativeDimensions()
+        {
+            OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(-4,-6);
+            assertEquals(-20, object.perimeterOfRectangle());
+        }
+
+
     }
-
-    @DisplayName("Area of Square with Positive Decimal side")
-    @Test
-    void toImplementAreaOfSquareWithPositiveDecimalSides()
-    {
-        OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(5.5);
-        assertEquals(30.25,object.areaOfSquare());
-    }
-
-    @DisplayName("Area of Square with Positive Decimal side")
-    @Test
-    void toImplementAreaOfSquareWithNegativeDecimalSides()
-    {
-        OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(-5.5);
-        assertEquals(30.25,object.areaOfSquare());
-    }
-
-    //Perimeter of Square
-    @DisplayName("Perimeter of Square with Positive side")
-    @Test
-    void toImplementPerimeterOfSquareWithPositiveSides()
-    {
-        OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(10);
-        assertEquals(40,object.perimeterOfSquare());
-    }
-
-    @DisplayName("Perimeter of Square with Negative side")
-    @Test
-    void toImplementPerimeterOfSquareWithNegativeSides()
-    {
-        OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(-10);
-        assertEquals(-40,object.perimeterOfSquare());
-    }
-
-    @DisplayName("Perimeter of Square with Positive Decimal side")
-    @Test
-    void toImplementPerimeterOfSquareWithPositiveDecimalSides()
-    {
-        OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(2.5);
-        assertEquals(10,object.perimeterOfSquare());
-    }
-
-    @DisplayName("Perimeter of Square with Negative Decimal side")
-    @Test
-    void toImplementPerimeterOfSquareWithNegativeDecimalSides()
-    {
-        OperationsOfSquareAndRectangleMain object = new OperationsOfSquareAndRectangleMain(-2.5);
-        assertEquals(-10,object.perimeterOfSquare());
-    }
-    //End Perimeter Of Square
-
-
 }
 
